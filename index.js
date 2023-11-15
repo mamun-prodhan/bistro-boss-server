a0221827b02d23aca645a47b1d26785650ff7d4c
mamun-prodhan <mamunpr9@gmail.com>
2023-11-16 00:47:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("bistro boss server is running");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
