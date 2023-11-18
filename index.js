705b385a4f6930e71d335c0b746e4ca5511626d4
mamun-prodhan <mamunpr9@gmail.com>
2023-11-18 12:03:22 +0600

    post cart data implemented

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
 
     const menuCollection = client.db("bistroDB").collection("menu");
     const reviewCollection = client.db("bistroDB").collection("reviews");
+    const cartCollection = client.db("bistroDB").collection("carts");
 
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
@@ -38,6 +39,13 @@ async function run() {
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
       console.log(result);
+      res.send(result);
+    });
+
+    // carts collection
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
       res.send(result);
     });
 
