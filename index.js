c70755781e74a349b88c2c0b22475ea76372bcea
mamun-prodhan <mamunpr9@gmail.com>
2023-11-19 22:56:53 +0600

    delete user api implemented

--- index.js
+++ index.js
@@ -32,6 +32,10 @@ async function run() {
     const cartCollection = client.db("bistroDB").collection("carts");
 
     // users related api
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
     app.post("/users", async (req, res) => {
       const user = req.body;
       // insert email if user dosen't exists:
@@ -45,6 +49,25 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // menu related api
     app.get("/menu", async (req, res) => {
       const result = await menuCollection.find().toArray();
       console.log(result);
